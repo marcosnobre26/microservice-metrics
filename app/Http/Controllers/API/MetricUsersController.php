fac7ee7150b6cab09106cd13cd493d7d189cfd5f
Marcos Nobre <marcosnobre26@gmail.com>
2022-12-09 13:17:18 -0300

    correção

--- app/Http/Controllers/API/MetricUsersController.php
+++ app/Http/Controllers/API/MetricUsersController.php
@@ -237,6 +237,11 @@ class MetricUsersController extends BaseController
         $data = [];
         $courses = MetricUsers::where('user_id', $id)->get();
 
+        foreach($courses as $item){
+            $course = Courses::where('id',$item->course_id)->first();
+            $item->course_name = $course->title;
+        }
+
         $completed_courses = MetricUsers::where('user_id', $id)->where('finished', 1)->get();
 
         $data['courses_completed'] = $completed_courses;
