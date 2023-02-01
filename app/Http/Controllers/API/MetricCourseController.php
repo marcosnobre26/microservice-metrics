ddfe88ff058d73c17b379d29558401d9f8b042f0
Marcos Nobre Castro Silva <marcosnobre26@gmail.com>
2023-02-01 01:44:25 -0300

    teste

--- app/Http/Controllers/API/MetricCourseController.php
+++ app/Http/Controllers/API/MetricCourseController.php
@@ -657,7 +657,7 @@ class MetricCourseController extends BaseController
         if($request->order === 'name-asc'){
 
             $users = User::leftJoin('user_subscription', 'user_subscription.user_id', '=', 'user.id')
-            //->leftJoin('user_subscription', 'user_subscription.package_id', '=', 'ead_class_module_subscription.package_id')
+            ->leftJoin('user_subscription', 'user_subscription.package_id', '=', 'ead_class_module_subscription.package_id')
             ->leftJoin('ead_class_module_subscription', 'ead_class_module_subscription.course_id', '=', 'course.id')
             
             //->leftJoin('users', 'users.id', '=', 'user_subscription.user_id')
