72d178069e282e49ee4b62603a9992417d6d56bb
Marcos Nobre Castro Silva <marcosnobre26@gmail.com>
2023-02-01 03:34:08 -0300

    teste

--- app/Http/Controllers/API/MetricCourseController.php
+++ app/Http/Controllers/API/MetricCourseController.php
@@ -636,7 +636,9 @@ class MetricCourseController extends BaseController
     public function classConsumed($id, $user_id){
         
         $classes = ClassesHistories::where('class_id', $id)->where('user_id', $user_id)->get();
-        
+        if($user_id === "d8fbd6d7-4ee6-453b-b812-8a4ba1122f5f"){
+            dd($id);
+        }
         
         $ponto = ':';
         $hora_um = "00:00:00";
